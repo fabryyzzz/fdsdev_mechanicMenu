e8509d4762ea2830816396c5b61f4fa94be2a130
Fabrizio - FDS Development <58892804+fabryyzzz@users.noreply.github.com>
2024-03-02 18:18:14 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+lua54 'yes'
+
+version '1.0'
+shared_script '@ox_lib/init.lua'
+
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    "server.lua"
+}
